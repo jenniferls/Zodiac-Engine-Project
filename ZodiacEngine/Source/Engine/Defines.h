360b10d8877f4c2765a9e946cdb020a37f4277c8
Jennifer Larsson-Ståhl <36172747+jenniferls@users.noreply.github.com>
2026-03-24 07:56:46 +0100

    Update code with wrappers for win32

--- ZodiacEngine/Source/Engine/Defines.h
+++ ZodiacEngine/Source/Engine/Defines.h
@@ -1,3 +1,5 @@
 #pragma once
 
-#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) //For detecting memory leaks
+#if defined(_MSC_VER_) && defined(_DEBUG)
+    #define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) //For detecting memory leaks
+#endif
