ad24d35d9fca41c8dabe071bcce5f12662dd0633
Jennifer Larsson-Ståhl <36172747+jenniferls@users.noreply.github.com>
2026-06-15 19:25:34 +0200

    Added VulkanTexture class

---
+++ ZodiacEngine/Source/Vulkan/VulkanTexture.cpp
@@ -0,0 +1,12 @@
+#include "Zodiacpch.h"
+#include "VulkanTexture.h"
+#include "Validation.h"
+#include "Initializers.h"
+
+Zodiac::VulkanTexture::VulkanTexture() {
+    
+}
+
+Zodiac::VulkanTexture::~VulkanTexture() {
+    
+}
---
+++ ZodiacEngine/Source/Vulkan/VulkanTexture.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vulkan/vulkan.h>
+
+#include "VulkanDevice.h"
+
+namespace Zodiac {
+	class VulkanTexture {
+	public:
+        VulkanTexture();
+		~VulkanTexture();
+
+	private:
+        VkImage m_image = VK_NULL_HANDLE;
+        VkDeviceMemory m_deviceMemory = VK_NULL_HANDLE;
+        VkImageView m_imageView = VK_NULL_HANDLE;
+        VkSampler m_sampler = VK_NULL_HANDLE;
+	};
+}
